72db863e5364a77889a9da0da9ee6e3acf5de506
Jay/Fienna Liang <roackb2@gmail.com>
2025-09-06 14:07:25 +0800

    Refactor & add ELK stack

--- src/infra/queue/QueuePort.ts
+++ src/infra/queue/QueuePort.ts
@@ -8,3 +8,59 @@ export abstract class QueuePort {
   abstract enqueue<T extends Record<string, any>>(queueName: string, item: QueueItem<T>): Promise<boolean>;
   abstract dequeue<T extends Record<string, any>>(queueName: string): Promise<QueueItem<T> | null>;
 }
+
+export abstract class IntegrationOperationPort {
+  // noop
+}
+
+// Credential management across all org
+export abstract class IntegrationCredentialManagerPort {
+  abstract loadCredential(): Promise<string>;
+  abstract clearCache(): Promise<void>;
+}
+
+
+
+
+class JiraOperationService implements IntegrationOperationPort {
+  private instance: AxiosInstance;
+
+  private buildAxiosOptions({
+    authType,
+    axiosOptions
+  }: {
+    authType: 'pat'
+    axiosOptions: string
+  }): AxiosRequestConfig {
+    if (authType === 'pat') {
+      const credential = await CustomerIntegrationService.injectCredential();
+    }
+  }
+
+  getInstance({
+    authType,
+    axiosOptions
+  }: {
+    authType: 'pat'
+    axiosOptions: string
+  }): Promise<AxiosInstance> {
+    return this.axiosInstance;
+  } {
+
+    if (authType === 'pat') {
+      const credential = await
+    }
+
+    const axiosInstance = axios.create({
+      baseURL: 'https://api.jira.com',
+      headers: {
+        'Authorization': `Bearer ${apiKey}`
+      }
+    })
+  }
+
+  getIssue(issueId: string): Promise<IssueType> {
+
+  }
+
+}
