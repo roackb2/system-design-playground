86a673044bc5637a1f9d945d97a4236837a800e1
Jay/Fienna Liang <roackb2@gmail.com>
2025-09-06 15:55:09 +0800

    Write application logs to local file and get harvested by filebeat
    - Normalize pino log output so that Kibana can pick it up

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,7 +1,20 @@
 import pino from "pino";
 
 const logger = pino({
-  level: process.env.LOG_LEVEL || 'info'
+  level: process.env.LOG_LEVEL || 'info',
+  transport: {
+    targets: [{
+      target: 'pino/file',
+      options: {
+        destination: 1, // write to stdout
+      }
+    }, {
+      target: 'pino/file',
+      options: {
+        destination: 'logs/app.json', // write to log file for filebeat to harvest
+      }
+    }]
+  }
 })
 logger.info('logger initialized')
 
